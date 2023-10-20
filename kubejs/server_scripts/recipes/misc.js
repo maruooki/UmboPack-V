2b606550cbc0bd25d9b2661d4554ae9fa03b5494
maruooki <maruooky@gmail.com>
2023-10-20 09:16:17 -0400

    revert pams, added some mods, attempted incontrol

--- kubejs/server_scripts/recipes/misc.js
+++ kubejs/server_scripts/recipes/misc.js
@@ -1,4 +1,5 @@
 // This script is for mods that are too little for their own JS file.
 ServerEvents.recipes(e => {
+    // Backpacks Mod
     e.remove({output: 'sophisticatedbackpacks:everlasting_upgrade'})
 })
